d96e679730a9b96bfa3b631666bdaf30db9ef429
Pierre Breny <pierre.breny@gmail.com>
2021-10-14 14:45:52 +0200

    added wrapAsync middleware to all campground endpoints

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,21 +5,21 @@ const wrapAsync = require('../utilities/wrapAsync');
 
 // Homepage with all campgrounds
 
-router.get('/', async (req,res) => {
+router.get('/', wrapAsync(async (req,res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds});
-})
+}))
 
-// GET Add campground
+// GET Add campground page
 router.get('/new', (req,res) => {
     res.render('campgrounds/new');
 })
 
 // Get campground by :id
-router.get('/:id', async (req,res) => {
+router.get('/:id', wrapAsync(async (req,res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground});
-})
+}))
 
 // Add new
 router.post('/', wrapAsync(async (req, res, next) => {
@@ -29,24 +29,24 @@ router.post('/', wrapAsync(async (req, res, next) => {
 }))
 
 // GET Edit campground page
-router.get('/:id/edit', async (req, res) => {
+router.get('/:id/edit', wrapAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', {campground});
-})
+}))
 
 // Edit campground
-router.put('/:id', async (req,res) => {
+router.put('/:id', wrapAsync(async (req,res) => {
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
 // GET Delete
-router.delete('/:id', async (req,res) => {
+router.delete('/:id', wrapAsync(async (req,res) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-})
+}))
 
 router.use((err, req, res, next) => {
     res.send("Something went wrong!")
